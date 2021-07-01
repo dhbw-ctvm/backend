88950949162e955bc24b5bd9113c92876c53e751
Noah Kilders <noah.kilders@sap.com>
2021-07-01 11:21:03 +0200

    Update RestToTheWorld.js

--- JS/RestToTheWorld.js
+++ JS/RestToTheWorld.js
@@ -1,32 +1,42 @@
 var express = require('express');
 var app = express();
-var fs = require("fs");
 var main = require('./main.js');
 var xmljs = require("xml-js");
 
-app.get('/getLocation/:location', function (req, res) {
-    console.log(req.params.location);
-    var paraString = req.params.location;
-    var point = paraString.split('S');
-    point[0] = point[0].replace('P', '.');
-    point[1] = point[1].replace('P', '.');
-    point[0] = parseFloat(point[0]);
-    point[1] = parseFloat(point[1]);
-    var data = main.startStack(point);
-    data = xmljs.json2xml(JSON.stringify(data), {compact: true, spaces: 4});
-    res.end((data));
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
 
-})
+app.get('/incidence', jsonParser, function (req, res) {
+    if(req.headers['content-type'] != 'application/json') {
+        res.status(415).send({error: 'content-type must be application/json'})
+        return
+    }
 
-app.get('/getLocationDebug', function (req, res) {
-    var data = main.startStack([8.407017, 49.014498])
-    res.end(JSON.stringify(data));
+    var data = main.startStack([
+        parseFloat(req.body.long),
+        parseFloat(req.body.lat)
+    ])
 
-})
+    // Damit das Rückgabe-XML einen Root-Tag hat
+    data = { xml: data }
 
-//http://127.0.0.1:8081/getLocation/8P407017S49P014498
+    // TODO: Filter, damit nur relevante Daten zurückgegeben werden
+
+    data = xmljs.json2xml(JSON.stringify(data), {compact: true, spaces: 4})
+
+    res.end(data)
+});
+
+app.get('/centers/test', function(req, res) {
+    // TODO: Array aller Testzentren zurückgeben
+});
+
+app.get('/centers/vaccination', function(req, res) {
+    // TODO: Array aller Impfzentren zurückgeben
+});
+
 var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log("Example app listening at http://%s:%s", host, port)
-})
+    console.log("Listening at http://%s:%s", host, port)
+});
