3d2dd810fbce8772aecb232c4e16f9628af54b4e
TheRaynex <rafael.luedtke@web.de>
2021-07-09 13:31:55 +0200

    testzentren machen chaos

--- JS/testcenterDataFormater.js
+++ JS/testcenterDataFormater.js
@@ -3,12 +3,23 @@ const fs = require('fs');
 let rawdata = fs.readFileSync('Data/testcenterDuesseldorf.json');
 let data = JSON.parse(rawdata);
 
-module.exports = {
-    testcenterData: function(){
-        var result;
+function getData(){
+    var resultf = '{ xml: '
         for (var i = 0; i < data.result.total; i++) {
-            result = result +data.result.records[i] ;
+            var result = data.result.records[i];
+            resultf = resultf + {
+                name: result['name_teststelle'],
+                addresse: result['standort_teststelle_strasse'] + ', ' + result['standort_teststelle_plz'] + ' ' + result['standort_teststelle_ort'],
+                koordinaten: { laenge: result['longitude'], breite: result['latitude'] },
+                terminbuchung: result['internet']
+            }
         }
-        return result;
+        resultf = resultf + '}'
+        return resultf;
+}
+
+module.exports = {
+    testcenterData: function () {
+        return getData();
     }
 }
